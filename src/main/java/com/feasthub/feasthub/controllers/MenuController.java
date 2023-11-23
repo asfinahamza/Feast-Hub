87ac258bf5df8fa7a418379e22886495a4ecc4cb
asfinahamza <asfinahamza@gmail.com>
2023-11-23 16:04:51 +0300

    Added view,update,delete for menu

--- src/main/java/com/feasthub/feasthub/controllers/MenuController.java
+++ src/main/java/com/feasthub/feasthub/controllers/MenuController.java
@@ -3,10 +3,13 @@ package com.feasthub.feasthub.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
 import com.feasthub.feasthub.entity.MenuItem;
 import com.feasthub.feasthub.repository.MenuRepository;
 import com.feasthub.feasthub.services.MenuService;
@@ -34,8 +37,34 @@ public class MenuController {
         Iterable<MenuItem> menuItems = menuService.getMenuItems();
         
         // Add menuItems to the model
-        model.addAttribute("menuItems", menuItems);
-
+        model.addAttribute("menuItems", menuItems); 
         return "index";
     }
+    
+    @GetMapping("/menu/{id}")
+    public String showMenuItem(@PathVariable Long id, Model model) {
+        Optional<MenuItem> menuItem = menuService.getMenuItemById(id);
+        model.addAttribute("menuItem", menuItem.orElse(null));
+        return "menuItem";
+    }
+
+    @PostMapping("/updateMenu/{id}")
+    public String updateMenu(@PathVariable Long id, @RequestParam String name, @RequestParam Float price) {
+        Optional<MenuItem> optionalMenuItem = menuService.getMenuItemById(id);
+
+        optionalMenuItem.ifPresent(menuItem -> {
+            menuItem.setName(name);
+            menuItem.setPrice(price);
+            menuRepository.save(menuItem);
+        });
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/deleteMenu/{id}")
+    public String deleteMenu(@PathVariable Long id) {
+        menuService.deleteMenuItemById(id);
+        return "redirect:/";
+    }
+    
 }
--- src/main/java/com/feasthub/feasthub/entity/MenuItem.java
+++ src/main/java/com/feasthub/feasthub/entity/MenuItem.java
@@ -1,6 +1,5 @@
 package com.feasthub.feasthub.entity;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
